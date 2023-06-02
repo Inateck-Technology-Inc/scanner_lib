2dfb12c3243835e2bb441a26a2b78cc6e5d5b6f1
Eileen <eileen@inateck.com>
2023-06-02 14:42:00 +0800

    init

--- Program.cs
+++ Program.cs
@@ -3,18 +3,22 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 
 namespace CsBindgen
 {
+    internal delegate void ConnectCallback(IntPtr result);
+
     internal static unsafe partial class NativeMethods
     {
-        const string __DllName = "scanner_c_abi.dll";
+        const string __DllName = "scanner_windows.dll";
 
         [DllImport(__DllName, EntryPoint = "scan", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         public static extern byte* scan();
 
         [DllImport(__DllName, EntryPoint = "connect", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern byte* connect(byte* device_id, byte* app_id, byte* developer_id, byte* app_key);
+        public static extern byte* connect(byte* device_id, byte* app_id, byte* developer_id, byte* app_key, ConnectCallback callback);
+
 
         [DllImport(__DllName, EntryPoint = "disconnect", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         public static extern byte* disconnect(byte* device_id);
@@ -35,7 +39,7 @@ namespace CsBindgen
         public static extern byte* get_basic_device_info(byte* device_id);
 
 
-
+    
 
         static void Main(string[] args)
         {
@@ -44,10 +48,11 @@ namespace CsBindgen
             Console.WriteLine(result);
 
           
-            byte[] deviceIdArray = Encoding.ASCII.GetBytes("***");
+            byte[] deviceIdArray = Encoding.ASCII.GetBytes("7B:AA:8C:98:10:71");
             byte[] appIdArray = Encoding.ASCII.GetBytes("***");
             byte[] developerIdArray = Encoding.ASCII.GetBytes("***");
             byte[] appKeyArray = Encoding.ASCII.GetBytes("***");
+
 
             unsafe
             {
@@ -56,13 +61,20 @@ namespace CsBindgen
                 fixed (byte* pDeveloperId = developerIdArray)
                 fixed (byte* pAppKey = appKeyArray)
                 {
-                    byte* functionResult = connect(pDeviceId, pAppId, pDeveloperId, pAppKey);
+                    byte* functionResult = connect(pDeviceId, pAppId, pDeveloperId, pAppKey, Callback);
                     result = new string((sbyte*)functionResult);
                     Console.WriteLine(result);
                 }
             }
+          
 
             Console.WriteLine("test end");
+        }
+
+        static void Callback(IntPtr result)
+        {
+            string resultString = Marshal.PtrToStringAnsi(result);
+            Console.WriteLine("Callback result: " + resultString);
         }
 
     }
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ The SDK provides developers with a set of tools to set the scanner for Windows(3
 To use a library in your project, there are generally the following steps:
 1. Add the "lib/*" library to your project;
 2. Import the "Program.cs" file in your project;
-3. Update the "__DllName" to the corresponding library name, such as libname = "scanner_c_abi.dll" in the "Program.cs" file;
+3. Update the "__DllName" to the corresponding library name, such as libname = "scanner_windows.dll" in the "Program.cs" file;
 
 ## Documentation
-For more information about the SDK and APIs, go to [http://docs.inateckoffice.com/web/#/8/24](http://docs.inateckoffice.com/web/#/8/24) .
+For more information about the SDK and APIs, go to [https://docs.inateck.com/scanner-api/](https://docs.inateck.com/scanner-api/) .
